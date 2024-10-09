6a0bb8ce43bc0dfb119cbf34b7e34662a8928d0d
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-10-09 06:51:44 +0530

    chore: routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,60 +1,75 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
+import {useFonts} from 'expo-font';
+import {Stack} from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 import 'react-native-reanimated';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import {useColorScheme} from '@/hooks/useColorScheme';
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+    const colorScheme = useColorScheme();
+    const [loaded] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
+    return (
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+            <Stack initialRouteName="login">
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack initialRouteName="login">
-        {/* login */}
-        <Stack.Screen name="login" options={{ headerShown: false }} />
-        <Stack.Screen name="login/otp" options={{ headerShown: false }} />
-        <Stack.Screen name="login/splash" options={{ headerShown: false }} />
+                {/* login */}
+                <Stack.Screen name="login" options={{headerShown: false}}/>
+                <Stack.Screen name="login/otp" options={{headerShown: false}}/>
+                <Stack.Screen name="login/splash" options={{headerShown: false}}/>
 
-        {/* sign up */}
-        <Stack.Screen name="signup" options={{ headerShown: false }} />
-        <Stack.Screen name="signup/otp" options={{ headerShown: false }} />
+                {/* signup */}
+                <Stack.Screen name="signup" options={{headerShown: false}}/>
+                <Stack.Screen name="signup/otp" options={{headerShown: false}}/>
 
-        {/* onboard */}
-        <Stack.Screen name="onboard/name" options={{ headerShown: false }} />
-        <Stack.Screen name="onboard/check-employed" options={{ headerShown: false }} />
-        <Stack.Screen name="onboard/check-status" options={{ headerShown: false }} />
-        <Stack.Screen name="onboard/employment-details" options={{ headerShown: false }} />
-        <Stack.Screen name="onboard/welcome" options={{ headerShown: false }} />
-        <Stack.Screen name="onboard/building" options={{ headerShown: false }} />
-        <Stack.Screen name="onboard/reveal-ladder" options={{ headerShown: false }} />
+                {/* onboard */}
+                <Stack.Screen name="onboard/name" options={{headerShown: false}}/>
+                <Stack.Screen name="onboard/check-employed" options={{headerShown: false}}/>
+                <Stack.Screen name="onboard/check-status" options={{headerShown: false}}/>
+                <Stack.Screen name="onboard/employment-details" options={{headerShown: false}}/>
+                <Stack.Screen name="onboard/welcome" options={{headerShown: false}}/>
+                <Stack.Screen name="onboard/building" options={{headerShown: false}}/>
+                <Stack.Screen name="onboard/reveal-ladder" options={{headerShown: false}}/>
 
-        <Stack.Screen name="questions/step1" options={{ headerShown: false }} />
-        <Stack.Screen name="questions/step2" options={{ headerShown: false }} />
-        <Stack.Screen name="questions/step3" options={{ headerShown: false }} />
-        <Stack.Screen name="questions/step4" options={{ headerShown: false }} />
-        <Stack.Screen name="questions/step5" options={{ headerShown: false }} />
+                {/* questions */}
+                <Stack.Screen name="questions/step1" options={{headerShown: false}}/>
+                <Stack.Screen name="questions/step2" options={{headerShown: false}}/>
+                <Stack.Screen name="questions/step3" options={{headerShown: false}}/>
+                <Stack.Screen name="questions/step4" options={{headerShown: false}}/>
+                <Stack.Screen name="questions/step5" options={{headerShown: false}}/>
 
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
-  );
+                {/* skills */}
+
+
+                {/* careers */}
+
+
+                {/* profile */}
+                <Stack.Screen name="profile/preferences" options={{headerShown: false}}/>
+
+                {/* tabs */}
+                <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+
+                {/* not found */}
+                <Stack.Screen name="+not-found"/>
+
+            </Stack>
+        </ThemeProvider>
+    );
 }
